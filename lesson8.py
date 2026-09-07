3e1333f8732d7d19eb155f1cb973c045b6863195
sheepy-sheepy <tereshchenkopolina0@mail.ru>
2026-09-07 20:23:57 +0300

    Функции с return

--- lesson8.py
+++ lesson8.py
@@ -6,3 +6,26 @@ def print_ticket(name, film):
 print_ticket("Sheepy", "1+1")
 print_ticket("Shrimp", "Ugly sister")
 print_ticket("Broccoli", "Batman")
+
+
+# 2
+def get_square(number):
+    return number ** 2
+
+
+square12 = get_square(12)
+print(square12 + 10)
+
+
+# 3
+def check_age(age):
+    if age < 12:
+        return "Ребенок"
+    elif age <= 17:
+        return "Подросток"
+    return "Взрослый"
+
+
+print(check_age(15))
+print(check_age(61))
+print(check_age(9))
