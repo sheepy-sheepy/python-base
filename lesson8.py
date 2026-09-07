f5876d49b6f5d82a48b5e7aff44ff754f49f3502
sheepy-sheepy <tereshchenkopolina0@mail.ru>
2026-09-07 20:10:50 +0300

    Функции

---
+++ lesson8.py
@@ -0,0 +1,8 @@
+# 1
+def print_ticket(name, film):
+    print(f"Билет для {name} на фильм {film}")
+
+
+print_ticket("Sheepy", "1+1")
+print_ticket("Shrimp", "Ugly sister")
+print_ticket("Broccoli", "Batman")
